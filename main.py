26339f150348d90db84e961e17e44a1720d851e3
citianalistamotta <analista@motta.com.br>
2025-09-17 12:21:40 -0400

    Ajustes em Historico

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
-from pywinauto import Application
-from pywinauto import findwindows
-from pywinauto.win32functions import PostMessage
-from pywinauto.win32defines import WM_CLOSE
 from datetime import datetime
 from historico import Historico
 from relatorio import Relatorio
+from enviadorWhats import EnviadorWhats
 
-
-def InicializaBot():
-    try:
-        chrome_window = findwindows.find_windows(title_re=".*Google Chrome.*")
-        if chrome_window:
-            app = Application().connect(title_re=".*Google Chrome")
-            chrome = app.top_window()
-            PostMessage(chrome_window[0], WM_CLOSE, 0, 0)
-        else:
-            print("Nenhum processo do Google Chrome em execução.")
-    except Exception as e:
-        print(f"Erro ao inicializar o bot: {e}")
 
 if __name__ == "__main__":
-        data_atual = datetime.now()
-        historico = Historico()
-        relatorio = Relatorio()
+    data_atual = datetime.now()
+    historico = Historico()
+    relatorio = Relatorio()
+    enviadorWhats = EnviadorWhats() 
 
-        relatorio.relatorioServicos() 
-        InicializaBot()
-        print("-----------------------------enviarRelatorioServicos-----------------------")
-        relatorio.enviarRelatorioServicos()
+    print("-----------------------------Historico-----------------------")
+    df_ordenado = historico.get_valorOrdenadoCompleto()
+    if df_ordenado is not None:
+        print("Execução finalizada.")
+    else:
+        print("Execução falhou.")
+    
+    print("-----------------------------Relatório-----------------------")
+    relatorio.relatorioServicos() 
 
-        
+    print("-----------------------------Enviar Relatório-----------------------")
+    enviadorWhats.enviar_relatorio()
+
